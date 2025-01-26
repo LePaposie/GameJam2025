7c45a09294161979c67fb44918ed5dc47e4cc3e7
Juan Sepulveda <lepap@pt-jsepulveda.ANTIOQUENA.local>
2025-01-26 15:55:09 -0500

    version final

--- Assets/_Game/Scripts/ExitScript.cs
+++ Assets/_Game/Scripts/ExitScript.cs
@@ -4,11 +4,11 @@ public class ExitScript : MonoBehaviour
 {
     public void ExitGame()
     {
-        // Si estamos en el editor de Unity
-        UnityEditor.EditorApplication.isPlaying = false;
+        //// Si estamos en el editor de Unity
+        //UnityEditor.EditorApplication.isPlaying = false;
 
-        // Si estamos ejecutando un build (ejecutable), cierra la aplicación
+        //// Si estamos ejecutando un build (ejecutable), cierra la aplicación
         Application.Quit();
-        Debug.Log("El juego se ha cerrado."); // Mensaje en la consola
+        //Debug.Log("El juego se ha cerrado."); // Mensaje en la consola
     }
 }
